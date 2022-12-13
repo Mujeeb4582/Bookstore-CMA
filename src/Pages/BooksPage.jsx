1f0d7c8822df8dc404ec92ce42a521d6af850145
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-13 14:45:52 +0500

    Add the Bookpage separately

---
+++ src/Pages/BooksPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookList from '../components/Books/BookList';
+import Form from '../components/Books/form';
+
+const BooksPage = () => (
+  <>
+    <BookList />
+    <Form />
+  </>
+);
+
+export default BooksPage;
