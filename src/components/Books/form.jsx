7d8125e4415567f504f232c0d8b831c2805db6b0
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-14 16:26:32 +0500

    Add the local state to the input

--- src/components/Books/form.jsx
+++ src/components/Books/form.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../../redux/books/books';
 
 const Form = () => {
   const dispatch = useDispatch();
+  const [bookTitle, setTitle] = useState('');
+  const [bookAuthor, setAuthor] = useState('');
 
   const handleForm = (e) => {
     e.preventDefault();
-    const bookTitle = e.target.elements.title.value;
-    const bookAuthor = e.target.elements.author.value;
-    const payload = { id: uuidv4(), bookTitle, bookAuthor };
-    dispatch(addBook(payload));
-    e.target.reset();
+    if (bookTitle.trim() !== '' && bookAuthor.trim() !== '') {
+      const payload = { id: uuidv4(), bookTitle, bookAuthor };
+      dispatch(addBook(payload));
+      setTitle('');
+      setAuthor('');
+    } else {
+      setTitle('');
+      setAuthor('');
+      alert('Please write book and author name.');
+    }
   };
 
   return (
@@ -20,8 +27,8 @@ const Form = () => {
       <h2 className="form-title">ADD NEW BOOK</h2>
 
       <form onSubmit={handleForm}>
-        <input type="text" name="title" placeholder="Book Title" />
-        <input type="text" name="author" placeholder="Book Author" />
+        <input type="text" name="title" value={bookTitle} onChange={(e) => setTitle(e.target.value)} placeholder="Book Title" />
+        <input type="text" name="author" value={bookAuthor} onChange={(e) => setAuthor(e.target.value)} placeholder="Book Author" />
         <button type="submit">ADD BOOK</button>
       </form>
     </div>
