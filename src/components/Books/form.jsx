3bbfb19a9e54a8125c3bc1777904d535b4974fb9
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-14 11:26:54 +0500

    Connect the form with redux-reducer

--- src/components/Books/form.jsx
+++ src/components/Books/form.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../../redux/books/books';
 
-const Form = () => (
-  <div className="form-container">
-    <h2 className="form-title">ADD NEW BOOK</h2>
+const Form = () => {
+  const dispatch = useDispatch();
 
-    <form>
-      <input type="text" placeholder="Book Title" />
-      <input type="text" placeholder="Book Author" />
-      <button type="submit">ADD BOOK</button>
-    </form>
+  const handleForm = (e) => {
+    e.preventDefault();
+    const bookTitle = e.target.elements.title.value;
+    const bookAuthor = e.target.elements.author.value;
+    const payload = { id: uuidv4(), bookTitle, bookAuthor };
+    dispatch(addBook(payload));
+    e.target.reset();
+  };
 
-  </div>
-);
+  return (
+    <div className="form-container">
+      <h2 className="form-title">ADD NEW BOOK</h2>
+
+      <form onSubmit={handleForm}>
+        <input type="text" name="title" placeholder="Book Title" />
+        <input type="text" name="author" placeholder="Book Author" />
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
