db46801b5717f2013880e85425207c9e43d18e7d
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-16 18:20:32 +0500

    Add the deployment setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Books/Navbar';
 import BooksPage from './Pages/BooksPage';
 import CategoryPage from './Pages/CategoryPage';
