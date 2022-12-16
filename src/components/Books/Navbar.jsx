3c4174e990edc53b9dc96d75a622041874be8fae
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-16 17:27:37 +0500

    Add the styling

--- src/components/Books/Navbar.jsx
+++ src/components/Books/Navbar.jsx
@@ -2,38 +2,26 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { CgProfile } from 'react-icons/cg';
 
-const Navbar = () => {
-  const links = [
-    {
-      id: 1,
-      path: '/',
-      text: 'BOOKS',
-    },
-    {
-      id: 2,
-      path: '/categories',
-      text: 'CATEGORIES',
-    },
-  ];
-  return (
-    <header>
-      <h1>Bookstore CMS</h1>
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link.id}>
-              <NavLink
-                to={link.path}
-              >
-                {link.text}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-        <button type="button" aria-label="Save" className="profile-icon"><CgProfile /></button>
-      </nav>
-    </header>
-  );
-};
-
+const Navbar = () => (
+  <header className="flex justify-between p-6 md:px-24 w-full shadow-lg min-h-24 bg-white">
+    <nav className="flex gap-10 flex-col md:flex-row">
+      <NavLink to="/">
+        <h1 className="h-full flex  items-center text-3xl font-bold text-azure mr-2">
+          Bookstore CMS
+        </h1>
+      </NavLink>
+      <NavLink to="/">
+        <span className="h-full flex items-center text-black-two text-sm">
+          BOOKS
+        </span>
+      </NavLink>
+      <NavLink to="/categories">
+        <span className="h-full flex items-center text-black-two text-sm opacity-50">
+          CATEGORIES
+        </span>
+      </NavLink>
+    </nav>
+    <button type="button" aria-label="Save" className="h-11 w-11 text-3xl flex items-center p-2 text-azure rounded-full border-2 border-grey-border"><CgProfile /></button>
+  </header>
+);
 export default Navbar;
